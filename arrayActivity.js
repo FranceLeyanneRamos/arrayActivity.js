5544c49de38f1bc0dea513fb47345ec3ac031ddd
FranceLeyanneRamos <ramosfranceleyanne@gmail.com>
2024-10-20 00:22:03 +0800

    First Commit

---
+++ arrayActivity.js
@@ -0,0 +1,15 @@
+//Create an array of favorite singers
+const favSingers = ["Chris Martin", "Taylor Swift", "Ari Staprans Leff"];
+console.log(favSingers[0]);
+
+//Create an array of favorite numbers
+const favNumbers = [4, 9, 14, 22];
+
+//Create a mixed array with different data types
+const mixedArr = ["string", ["other array"], 123, true];
+
+//Access and log each item in the mixedArr
+console.log(mixedArr[0]);
+console.log(mixedArr[1]);
+console.log(mixedArr[2]);
+console.log(mixedArr[3]);
